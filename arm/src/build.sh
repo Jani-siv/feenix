86861916bb0fcd1c117663063cf4bf3263a147a3
Jani Sivonen <info@kengityspaja.fi>
2022-11-03 19:24:55 +0200

    Trying get memory aligment corrected with linker script

--- arm/src/build.sh
+++ arm/src/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-arm-none-eabi-as -o startup.o startup.s
-arm-none-eabi-ld -T linkscript.ld -o test.elf startup.o
-arm-none-eabi-objcopy -O binary test.elf test.bin
+NAME="boot"
+
+arm-none-eabi-gcc -c -nostdlib -nostartfiles -lgcc -o cstart.o cstart.c
+arm-none-eabi-ld -T linkscript.ld -o ${NAME}.elf cstart.o
+arm-none-eabi-objcopy -O binary ${NAME}.elf ${NAME}.bin
+arm-none-eabi-objdump -S ${NAME}.elf > ${NAME}1.list
+arm-none-eabi-objdump -d .data ${NAME}.elf
+#arm-none-eabi-gcc -c -nostdlib -lgcc -nostartfiles cstart.c -o ${NAME}.o
+#arm-none-eabi-ld  -T linkscript.ld  -o ${NAME}.elf ${NAME}.o startup.o
+#arm-none-eabi-objcopy -O binary ${NAME}.elf ${NAME}.bin
+#arm-none-eabi-objdump -S ${NAME}.elf > ${NAME}.list
+
--- arm/src/cstart.c
+++ arm/src/cstart.c
@@ -1,7 +1,60 @@
-//empty
-
-int main()
+#define STACK_TOP 0x20000800
+#define NVIC_CCR ((volatile unsigned long *)(0xE000ED14))
+// Declare functions
+void myputs(char *string1);
+void myputc(char mychar);
+int main(void);
+void nmi_handler(void);
+void hardfault_handler(void);
+// Define the vector table
+__attribute__ ((section("vectors")))
+void (* const VectorArray[])(void) = {
+    (void (*)(void)) STACK_TOP,
+(void (*)(void)) main,
+nmi_handler,
+hardfault_handler
+};
+// Start of main program
+int main(void)
 {
+    const char *helloworld[]={"Hello world\n"};
+    *NVIC_CCR = *NVIC_CCR | 0x200; /* Set STKALIGN in NVIC */
+    myputs(*helloworld);
     while(1);
-    return 0;
+    return(0);
+}
+// Functions
+void myputs(char *string1)
+{
+    char mychar;
+    int j;
+    j=0;
+    do {
+        mychar = string1[j];
+        if (mychar!=0) {
+            myputc(mychar);
+            j++;
+        }
+    } while (mychar != 0);
+    return;
+}
+
+void myputc(char mychar)
+{
+#define UART0_DATA ((volatile unsigned long *)(0x4000C000))
+#define UART0_FLAG ((volatile unsigned long *)(0x4000C018))
+// Wait until busy flag is clear
+    while ((*UART0_FLAG & 0x20) != 0);
+// Output character to UART
+    *UART0_DATA = mychar;
+    return;
+}
+//Dummy handlers
+void nmi_handler(void)
+{
+    return;
+}
+void hardfault_handler(void)
+{
+    return;
 }
