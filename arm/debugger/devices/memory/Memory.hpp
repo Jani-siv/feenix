6f57be0c0156fd14fa710c1a367cc3dfe0fd2527
Jani Sivonen <info@kengityspaja.fi>
2022-11-07 19:45:15 +0200

    begin of file loader

--- arm/debugger/devices/memory/Memory.hpp
+++ arm/debugger/devices/memory/Memory.hpp
@@ -19,7 +19,7 @@ public:
     uint32_t ReadMemory32(uint32_t address);
     uint16_t ReadMemory16(uint32_t address);
     uint8_t ReadMemory8(uint32_t address);
-    uint32_t GetMemorySize() {return memorySize_;}
+    [[nodiscard]] uint32_t GetMemorySize() const {return memorySize_;}
 private:
     std::string permission_{};
     uint32_t memorySize_{};
