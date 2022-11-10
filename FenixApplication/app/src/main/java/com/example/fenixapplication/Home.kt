9e38d05cc39a8c83c2c1a320cfdff2263b0e04cf
Alexander Sihvo <mealex.sihvo@gmail.com>
2022-11-10 20:09:18 +0200

    RecyclerView and Fragments now works with together. Home-fragment is almost done

--- FenixApplication/app/src/main/java/com/example/fenixapplication/Home.kt
+++ FenixApplication/app/src/main/java/com/example/fenixapplication/Home.kt
@@ -10,8 +10,10 @@ import androidx.recyclerview.widget.RecyclerView
 
 class Home : Fragment(R.layout.fragment_home) {
 
-    private lateinit var newRecyclerView: RecyclerView
-    private lateinit var newArrayList: ArrayList<Devices>
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: RecyclerAdapter
+    private lateinit var devicesArrayList: ArrayList<Devices>
+
     lateinit var deviceName : Array<String>
     lateinit var deviceStatus : Array<String>
     lateinit var deviceLastActivity : Array<String>
@@ -22,25 +24,29 @@ class Home : Fragment(R.layout.fragment_home) {
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_home, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataInitialize()
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        adapter = RecyclerAdapter(devicesArrayList)
+        recyclerView.adapter = adapter
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private fun dataInitialize() {
+        devicesArrayList = arrayListOf<Devices>()
         deviceName = arrayOf("#1", "#2", "#3")
         deviceStatus = arrayOf("#1 S", "#2 S", "#3 S")
         deviceLastActivity = arrayOf("#1 L", "#2 L", "#3 L")
 
-        newArrayList = arrayListOf<Devices>()
-        getDeviceData()
-
-    }
-
-    private fun getDeviceData() {
         for(i in deviceName.indices) {
             val devices = Devices(deviceName[i], deviceStatus[i], deviceLastActivity[i])
-            newArrayList.add(devices)
+            devicesArrayList.add(devices)
         }
-        newRecyclerView.adapter = RecyclerAdapter(newArrayList)
     }
+
+
 
 }
