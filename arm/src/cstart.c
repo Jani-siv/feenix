91999eb38a32d748e068ae03c49cd04330442b30
Jani Sivonen <info@kengityspaja.fi>
2022-11-28 19:02:56 +0200

    fixed c file

--- arm/src/cstart.c
+++ arm/src/cstart.c
@@ -1,23 +1,13 @@
 #include <stdint.h>
 #define UART0_DATA ((volatile unsigned long *)(0x4000C000))
 #define UART0_FLAG ((volatile unsigned long *)(0x4000C018))
+#define NVIC_CCR ((volatile unsigned long *)(0xE000ED14))
+
 extern uint8_t _sbss;
 extern uint8_t _ebss;
 volatile uint8_t *data = &_sbss;
 volatile uint8_t *data_end = &_ebss;
-/*
-int main() {
-    while(data < data_end)
-    {
-        data = 0x0;
-        data++;
-    }
-    while(1);
-    return 0;
-}
- */
 
-#define NVIC_CCR ((volatile unsigned long *)(0xE000ED14))
 // Declare functions
 void myputs(char *string1);
 void myputc(char mychar);
